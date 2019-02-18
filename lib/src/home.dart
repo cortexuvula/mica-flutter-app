9edfc4fb326645d8ad344849487b2787308b838d
Andre Hugo <cortexpeterpan@gmail.com>
2019-02-18 15:18:41 -0800

    Patient information page completed

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -3,7 +3,6 @@ import 'package:after_layout/after_layout.dart';
 import 'package:mica/resources/const_data.dart' as appData;
 
 class Home extends StatefulWidget {
-
   bool viewedDisclaimer;
 
   Home({Key key, this.viewedDisclaimer}) : super(key: key);
@@ -15,13 +14,89 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> with AfterLayoutMixin<Home> {
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
-        body: Center(
-          child: Container(
-            child: Text("Midland Cognitive Assessment"),
-          ),
+        appBar: AppBar(
+          title: Text(appData.appName),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: <Widget>[
+            Container(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 25.0,
+                ),
+                Container(
+                  width: width * 0.9,
+                  child: Card(
+                    elevation: 10.0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(appData.appDescription,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 14.0,
+                              color: Colors.black)),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: width * 0.9,
+                  child: Card(
+                    elevation: 10.0,
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: <Widget>[
+                          RaisedButton(
+                            elevation: 10.0,
+                            onPressed: () {
+                              Navigator.pushNamed(
+                                  context, "/patient_information");
+                            },
+                            child: Text(appData.fullTestButton),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            appData.fullTestText,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontSize: 14.0,
+                                color: Colors.black),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: width * 0.9,
+                  child: Card(
+                    elevation: 10.0,
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: RaisedButton(
+                        elevation: 10.0,
+                        onPressed: () => debugPrint("Clicked pdf button"),
+                        child: Text(appData.bookletButton),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ))
+          ],
         ),
       ),
     );
@@ -29,8 +104,6 @@ class _HomeState extends State<Home> with AfterLayoutMixin<Home> {
 
   @override
   void afterFirstLayout(BuildContext context) async {
-
-
     if (!widget.viewedDisclaimer) {
       showDialog(
           barrierDismissible: false,
@@ -39,6 +112,7 @@ class _HomeState extends State<Home> with AfterLayoutMixin<Home> {
             return WillPopScope(
               onWillPop: () async => false,
               child: AlertDialog(
+                backgroundColor: Colors.white,
                 title: Text(
                   "Disclaimer",
                   style: TextStyle(
@@ -48,15 +122,17 @@ class _HomeState extends State<Home> with AfterLayoutMixin<Home> {
                 content: Text(appData.disclaimer),
                 actions: <Widget>[
                   FlatButton(
-                      color: Colors.white,
+                      color: Theme.of(context).primaryColor,
                       textColor: Colors.black,
                       shape: RoundedRectangleBorder(
                           borderRadius: new BorderRadius.circular(30.0)),
                       onPressed: () {
-
                         Navigator.of(context).pop();
                       },
-                      child: Text("I Agree")),
+                      child: Text(
+                        "I Agree",
+                        style: TextStyle(color: Colors.white),
+                      )),
                 ],
               ),
             );
