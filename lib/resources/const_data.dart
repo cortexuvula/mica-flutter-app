743ddfe8e28bbfd23d8ad4226fb5f7b34d04448b
Andre Hugo <cortexpeterpan@gmail.com>
2019-02-18 18:30:21 -0800

    Language Comprehension completed

--- lib/resources/const_data.dart
+++ lib/resources/const_data.dart
@@ -7,3 +7,11 @@ const String bookletButton = "BCA Booklet";
 const String fullTestButton = "Start Full Test";
 const String fullTestText = "Ensure that all testing is done in a quiet environment without any disturbance. You will need two blank sheets of A4 paper and a pen or pencil.";
 const String startTesting = "We are now going to do some tests of your memory and thinking. Some of the tests will be easy and others difficult. Just try your best. Any questions?";
+
+const String testLanguageComprehension = "Language Comprehension";
+const String testLanguageComprehensionDetails = "Evaluate comprehension of spoken language by using a 3-stage command.";
+const String testLanguageComprehensionToPatient = "Can you point with your finger to the ceiling, floor and the door.";
+const String testLanguageComprehensionResponse = "Note the response of the patient and score as below.";
+const String testLanguageComprehensionResponseNormal = "Normal = 3 stages complete without an error";
+const String testLanguageComprehensionResponseEquivocal = "Equivocal = some difficulty, 1-2 stages completed";
+const String testLanguageComprehensionResponseImpaired = "Impaired = 1 or more clear errors";
