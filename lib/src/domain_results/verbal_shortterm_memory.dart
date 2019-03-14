2285403b7dd3018741675e9bafd2a654ca42443c
Andre Hugo <cortexpeterpan@gmail.com>
2019-03-13 22:07:30 -0700

    Report completed

--- lib/src/domain_results/verbal_shortterm_memory.dart
+++ lib/src/domain_results/verbal_shortterm_memory.dart
@@ -23,6 +23,33 @@ class VerbalShortTermMemory extends StatefulWidget {
 }
 
 class _VerbalShortTermMemoryState extends State<VerbalShortTermMemory> {
+
+  String resultTrial3 = "normal";
+  Color resultTrial3Color;
+
+  String resultTrial2 = "normal";
+  Color resultTrial2Color;
+
+  String resultOrientation = "normal";
+  Color resultOrientationColor;
+
+  String resultDelay = "normal";
+  Color resultDelayColor;
+
+  String resultRecon = "normal";
+  Color resultReconColor;
+
+
+  @override
+  void initState() {
+    super.initState();
+    valueTrial2ResultToString(widget.trialTwoScore);
+    valueTrial3ResultToString(widget.trialThreeScore);
+    valueOrientationResultToString(widget.orientation);
+    valueRecallResultToString(widget.tenWordDelay);
+    valueRecogResultToString(widget.scoreVerbalRecognitionMemoryTenWords);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,70 +76,70 @@ class _VerbalShortTermMemoryState extends State<VerbalShortTermMemory> {
       body: ListView(
         children: <Widget>[
           Card(
-            color: Colors.green,
+            color: resultTrial2Color,
             elevation: 10.0,
             child: ListTile(
               title: Text("Short-term Memory Verbal Trial 2",style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w500,
               ),),
-              trailing: Text("${widget.trialTwoScore}",style: TextStyle(
+              trailing: Text(resultTrial2,style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w500,
               ),),
             ),
           ),
           Card(
-            color: Colors.green,
+            color: resultTrial3Color,
             elevation: 10.0,
             child: ListTile(
               title: Text("Short-term Memory Verbal Trial 3",style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w500,
               ),),
-              trailing: Text("${widget.trialThreeScore}",style: TextStyle(
+              trailing: Text(resultTrial3,style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w500,
               ),),
             ),
           ),
           Card(
-            color: Colors.green,
+            color: resultOrientationColor,
             elevation: 10.0,
             child: ListTile(
               title: Text("Orientation",style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w500,
               ),),
-              trailing: Text("${widget.orientation}",style: TextStyle(
+              trailing: Text(resultOrientation,style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w500,
               ),),
             ),
           ),
           Card(
-            color: Colors.green,
+            color: resultDelayColor,
             elevation: 10.0,
             child: ListTile(
               title: Text("Short-Term Memory Verbal: Delayed Recall Of 10 Words",style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w500,
               ),),
-              trailing: Text("${widget.tenWordDelay}",style: TextStyle(
+              trailing: Text(resultDelay,style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w500,
               ),),
             ),
           ),
           Card(
-            color: Colors.green,
+            color: resultReconColor,
             elevation: 10.0,
             child: ListTile(
               title: Text("Short-Term Memory Verbal Recognition: Total Score",style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w500,
               ),),
-              trailing: Text("${widget.scoreVerbalRecognitionMemoryTenWords}",style: TextStyle(
+              trailing: Text(resultRecon,style: TextStyle(
                 color: Colors.black,
                 fontWeight: FontWeight.w500,
               ),),
@@ -121,5 +148,139 @@ class _VerbalShortTermMemoryState extends State<VerbalShortTermMemory> {
         ],
       ),
     );
+  }
+
+  String valueTrial2ResultToString(int _valueTotal) {
+    switch (_valueTotal > 6) {
+      case true: {
+        setState(() {
+          resultTrial2 = "Normal";
+          resultTrial2Color = Colors.green;
+        });
+        break;
+      }
+      case false: {
+        if (_valueTotal < 5) {
+          setState(() {
+            resultTrial2 = "Impaired";
+            resultTrial2Color = Colors.red;
+          });
+          break;
+        }
+        setState(() {
+          resultTrial2 = "Equivocal";
+          resultTrial2Color = Colors.yellow;
+        });
+        break;
+      }
+
+    }
+  }
+
+  String valueTrial3ResultToString(int _valueTotal) {
+    switch (_valueTotal > 7) {
+      case true: {
+        setState(() {
+          resultTrial3 = "Normal";
+          resultTrial3Color = Colors.green;
+        });
+        break;
+      }
+      case false: {
+        if (_valueTotal < 5) {
+          setState(() {
+            resultTrial3 = "Impaired";
+            resultTrial3Color = Colors.red;
+          });
+          break;
+        }
+        setState(() {
+          resultTrial3 = "Equivocal";
+          resultTrial3Color = Colors.yellow;
+        });
+        break;
+      }
+
+    }
+  }
+
+  String valueOrientationResultToString(int _valueTotal) {
+    switch (_valueTotal > 4) {
+      case true: {
+        setState(() {
+          resultOrientation = "Normal";
+          resultOrientationColor = Colors.green;
+        });
+        break;
+      }
+      case false: {
+        if (_valueTotal < 4) {
+          setState(() {
+            resultOrientation = "Impaired";
+            resultOrientationColor = Colors.red;
+          });
+          break;
+        }
+        setState(() {
+          resultOrientation = "Equivocal";
+          resultOrientationColor = Colors.yellow;
+        });
+        break;
+      }
+
+    }
+  }
+
+  String valueRecallResultToString(int _valueTotal) {
+    switch (_valueTotal > 5) {
+      case true: {
+        setState(() {
+          resultDelay = "Normal";
+          resultDelayColor = Colors.green;
+        });
+        break;
+      }
+      case false: {
+        if (_valueTotal < 5) {
+          setState(() {
+            resultDelay = "Impaired";
+            resultDelayColor = Colors.red;
+          });
+          break;
+        }
+        setState(() {
+          resultDelay = "Equivocal";
+          resultDelayColor = Colors.yellow;
+        });
+        break;
+      }
+
+    }
+  }
+  String valueRecogResultToString(int _valueTotal) {
+    switch (_valueTotal > 5) {
+      case true: {
+        setState(() {
+          resultRecon = "Normal";
+          resultReconColor = Colors.green;
+        });
+        break;
+      }
+      case false: {
+        if (_valueTotal < 5) {
+          setState(() {
+            resultRecon = "Impaired";
+            resultReconColor = Colors.red;
+          });
+          break;
+        }
+        setState(() {
+          resultRecon = "Equivocal";
+          resultReconColor = Colors.yellow;
+        });
+        break;
+      }
+
+    }
   }
 }
